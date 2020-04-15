b7fcd73e0fc192623ec6d105239da72a977a6555
Sankalp Pund <sankalp.pund@colorado.edu>
2020-04-15 04:46:51 -0700

    python testing

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,8 +6,8 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_VERSION = 459304c70295ef225b1fee7c0e1c688fa71f818f
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/s20-remote-health-monitoring.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
 
@@ -17,7 +17,8 @@ endef
 
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/Heart_Rate_30102/heart_rate_application $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/tmp102/tmp102 $(TARGET_DIR)/usr/bin
 endef
 
 
