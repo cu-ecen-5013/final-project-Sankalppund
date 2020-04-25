efa08663138e4a08863334ab8cc72692ea4bbb92
Sankalp Pund <sankalp.pund@colorado.edu>
2020-04-24 17:07:12 -0700

    HR application update

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 4fc57949bd5c0aa1711f62ccefe55b954a21cd69
+AESD_ASSIGNMENTS_VERSION = 83cf1e9d807a6a5b066708b97048a53eb0fd1503
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/s20-remote-health-monitoring.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -17,7 +17,7 @@ endef
 
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/Heart_Rate_30102/heart_rate_application $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/Heart_Rate_30102/HR_application $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/tmp102/tmp102 $(TARGET_DIR)/usr/bin
 endef
 
